b7cea2b28c25db2f3b5711d2151c529ebedb51ee
Ayush22 <ayushkumar05827@gmail.com>
2026-07-07 22:58:41 +0530

    Initialized Currency Converter Project

---
+++ 06currencyConverter/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-white text-3xl text-center p-3'>Currency Convertor</h1>
+    </>
+  )
+}
+
+export default App;
