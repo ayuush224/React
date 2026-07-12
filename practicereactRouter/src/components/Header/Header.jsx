f1daa8da4cc8057735065b8b08e304b48ae60c29
Ayush22 <ayushkumar05827@gmail.com>
2026-07-13 03:02:53 +0530

    Finalized the header component

--- practicereactRouter/src/components/Header/Header.jsx
+++ practicereactRouter/src/components/Header/Header.jsx
@@ -4,9 +4,51 @@ import {Link, NavLink} from "react-router";
 function Header(){
     return (
         <>
-        <h1 className="text-black bg-orange-600 text-3xl text-center p-3">
-            Header
-        </h1>
+        <ul>
+            <div className="flex justify-center gap-8 border-b-2 bg-[#FAFAFA] border-b-[#E8EDF2]
+            py-2">
+                <li className="border rounded p-1 cursor-pointer bg-[#E8EDF2] min-w-20 text-center
+                transition-all duration-300 ease-linear hover:scale-120">
+                    <NavLink
+                        to="/"
+                        className={({isActive}) => {
+                            return `${isActive ? "text-[#2FA4D7]" : "text-gray-600"}`
+                        }}>
+                            Home
+                    </NavLink>
+                </li>
+
+                <li className="border rounded p-1 cursor-pointer bg-[#E8EDF2] min-w-20 text-center
+                ransition-all duration-300 ease-linear hover:scale-120">
+                        <NavLink to="/about"
+                        className={({isActive}) => {
+                            return `${isActive ? "text-[#2FA4D7]" : "text-gray-600"}`
+                        }}>
+                            About
+                    </NavLink>
+                </li>
+                
+                <li className="border rounded p-1 cursor-pointer bg-[#E8EDF2] min-w-20 text-center
+                ransition-all duration-300 ease-linear hover:scale-120">
+                        <NavLink to="/contact"
+                        className={({isActive}) => {
+                            return `${isActive ? "text-[#2FA4D7]" : "text-gray-600"}`
+                        }}>
+                            Contact
+                    </NavLink>
+                </li>
+
+                <li className="border rounded p-1 cursor-pointer bg-[#E8EDF2] min-w-20 text-center
+                ransition-all duration-300 ease-linear hover:scale-120">
+                        <NavLink to="/github"
+                        className={({isActive}) => {
+                            return `${isActive ? "text-[#2FA4D7]" : "text-gray-600"}`
+                        }}>
+                            Github
+                    </NavLink>
+                </li>
+            </div>
+        </ul>
         </>
     )
 }
--- practicereactRouter/src/components/Layout/Layout.jsx
+++ practicereactRouter/src/components/Layout/Layout.jsx
@@ -7,7 +7,6 @@ function Layout(){
     return (
         <>
             <Header/>
-            <Footer/>
         </>
     )
 }
