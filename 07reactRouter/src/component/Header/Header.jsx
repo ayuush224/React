6e2125a0a390dc259ec159d7944aa583c9480a3f
Ayush22 <ayushkumar05827@gmail.com>
2026-07-12 23:56:08 +0530

    Added a peinding style in the github page

--- 07reactRouter/src/component/Header/Header.jsx
+++ 07reactRouter/src/component/Header/Header.jsx
@@ -77,10 +77,11 @@ export default function Header() {
                             <li>
                                 <NavLink
                                     to="/github"
-                                    className={({isActive}) =>
+                                    className={({isActive, isPending}) =>
                                         `block py-2 pr-4 pl-3 duration-200 border-b
                                         border-gray-100 hover:bg-gray-50
                                         ${isActive ? "text-orange-700" : "text-gray-700"}
+                                        ${isPending ? "text-blue-700" : "text-gray-700"}
                                         lg:hover:bg-transparent lg:border-0
                                         hover:text-orange-700 lg:p-0`
                                     }
