450f2668ab89b0afd587f37e1443f8ef364b4cef
Ayush22 <ayushkumar05827@gmail.com>
2026-07-13 16:02:20 +0530

    Finalized Everythin & Project is Done, Learned React router by building this project

--- practicereactRouter/src/main.jsx
+++ practicereactRouter/src/main.jsx
@@ -52,9 +52,8 @@ const router = createBrowserRouter(
   )
 )
 
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router}/>
-  </StrictMode>,
+  </StrictMode>
 )
