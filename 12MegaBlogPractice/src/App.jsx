c850f486ab7acd57e0dd96f477cf6d56069cc903
Ayush22 <ayushkumar05827@gmail.com>
2026-07-18 12:54:23 +0530

    Initialized MegaBlog Practice Project

---
+++ 12MegaBlogPractice/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='text-center h-screen bg-[#212121] text-white
+    text-2xl p-4'>
+      Mega Project Practice
+    </div>
+    </>
+  )
+}
+
+export default App
