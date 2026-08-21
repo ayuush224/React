db1da69899312e7aa8a1486726a443905c200dcd
Ayush22 <ayushkumar05827@gmail.com>
2026-08-22 01:26:36 +0530

    Revised creating custom react project

--- RevisionReact/01vitereact/src/App.jsx
+++ RevisionReact/01vitereact/src/App.jsx
@@ -1,7 +1,11 @@
 function App() {
+  const username = "chai aur code";
   return (
     <>
-      <h1>Chai aur react with vite | Ayush Kumar</h1>
+      <h1>
+        Chai aur react {username}
+        {/* we write evaluated expresssion inside this block, not the js if for while loops, we write the result inside this blocks*/}
+      </h1>
     </>
   )
 }
--- RevisionReact/01vitereact/src/main.jsx
+++ RevisionReact/01vitereact/src/main.jsx
@@ -1,7 +1,50 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import React from 'react';
+
+
+// there is no need to export this function, but just to remove the red lines
+// i have exported this fucntion
+export function MyApp(){
+  return (
+    <div>
+      <h1>Custom App !</h1>
+    </div>
+  )
+}
+
+//this will not work here because vite does not work the same way for the react element
+//there is some other way in which it converts an jsx to react element
+// const ReactElement = {
+//     type : 'a',
+//     props : {
+//         href : "https:/google.com",
+//         target : "_blank"
+//     },
+//     children : "Click me to visit google"
+// };
+
+const anotherElement = (
+  <a href='https://google.com' target='_blank'>Visit Google</a>
+);
+
+const anotherUser = "Chai aur react"
+// it is the defined way of the vite library for writing a reactElement object
+const reactElement = React.createElement(
+  'a',
+  {href : 'https://google.com', target : "_blank"},
+  'Click me to visit google',
+  anotherUser
+);
 
 createRoot(document.getElementById('root'))
 .render(
-  <App />
+  <>
+    <App />
+    <MyApp />
+    {MyApp()}
+    {anotherElement}
+    {<div></div>} {/* // just adding to break the line */}
+    {reactElement}
+  </>
 )
