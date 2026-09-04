a62988184d2ff7e0aafbbcaed77ee06c7c7a0d96
Ayush22 <ayushkumar05827@gmail.com>
2026-09-04 22:57:58 +0530

    Initialized the currency Convertor project

---
+++ RevisionReact/06currencyConvertor/src/App.jsx
@@ -0,0 +1,11 @@
+function App(){
+  return (
+    <div className="min-h-screen bg-[#212121]">
+      <h1 className="text-center text-3xl font-bold text-white bg-[#aaaa] py-2 border-b-[#eeee] border-b">
+        Chai aur react
+      </h1>
+    </div>
+  )
+}
+
+export default App
